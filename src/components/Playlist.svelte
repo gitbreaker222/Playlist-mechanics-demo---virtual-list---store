6e14bb2f312588132a6e8ce8ea74fca8a13fc6ad
Ruben L <dummy@email.address>
2021-05-13 00:55:45 +0200

    uses immutable option

--- src/components/Playlist.svelte
+++ src/components/Playlist.svelte
@@ -1,54 +1,56 @@
-<script>
-	import { onMount, afterUpdate, tick } from "svelte";
-	// import { flip } from "svelte/animate";
-	// import { fade } from "svelte/transition";
-	// import VirtualList from '@sveltejs/svelte-virtual-list';
-	import VirtualList from "./VirtualList.svelte";
-	import {
-		playerStore,
-		playPause,
-		play,
-		queueSong,
-		resetSong,
-		jumpTo,
-		PLAYED,
-		CURRENT,
-		QUEUE,
-		PREV_QUEUE,
-		REMAINING,
-	} from "../store/playerStore.js";
-	import {
-		// send,
-		// receive,
-		filterListByName,
-		addType,
-	} from "../utils.js";
+<svelte:options immutable={true} />
 
-	// Constants / Variables
-	let autoscroll = false;
-	let scrollToIndex;
+<script>
+  import { onMount, afterUpdate, tick } from "svelte";
+  // import { flip } from "svelte/animate";
+  // import { fade } from "svelte/transition";
+  // import VirtualList from '@sveltejs/svelte-virtual-list';
+  import VirtualList from "./VirtualList.svelte";
+  import {
+    playerStore,
+    playPause,
+    play,
+    queueSong,
+    resetSong,
+    jumpTo,
+    PLAYED,
+    CURRENT,
+    QUEUE,
+    PREV_QUEUE,
+    REMAINING,
+  } from "../store/playerStore.js";
+  import {
+    // send,
+    // receive,
+    filterListByName,
+    addType,
+  } from "../utils.js";
+
+  // Constants / Variables
+  let autoscroll = false;
+  let scrollToIndex;
   let liStart = undefined;
   let liEnd = undefined;
 
-	$: isPaused = $playerStore.paused;
-	$: filterText = $playerStore.filterText;
-	$: previous = $playerStore.previous;
-	$: current = $playerStore.current;
-	$: next = $playerStore.next;
-	$: nextPrev = $playerStore.nextPrev;
-	$: remaining = $playerStore.remaining;
+  $: isPaused = $playerStore.paused;
+  $: filterText = $playerStore.filterText;
+  $: previous = $playerStore.previous;
+  $: current = $playerStore.current;
+  $: next = $playerStore.next;
+  $: nextPrev = $playerStore.nextPrev;
+  $: remaining = $playerStore.remaining;
 
-	// Derived
-	$: completeList = filterListByName(
-		[
-			...previous.map((i) => addType(i, PLAYED)),
-			addType(current, CURRENT),
-			...next.map((i) => addType(i, QUEUE)),
-			...nextPrev.map((i) => addType(i, PREV_QUEUE)),
-			...remaining.map((i) => addType(i, REMAINING)),
-		],
-		filterText
-	);
+  // Derived
+  $: completeList = filterListByName(
+    [
+      ...previous.map((i) => addType(i, PLAYED)),
+      addType(current, CURRENT),
+      ...next.map((i) => addType(i, QUEUE)),
+      ...nextPrev.map((i) => addType(i, PREV_QUEUE)),
+      ...remaining.map((i) => addType(i, REMAINING)),
+    ],
+    filterText
+  );
 
   $: {
     [current].map(() => {
@@ -56,36 +58,36 @@
     });
   }
 
-	function scrollToCurrent() {
+  function scrollToCurrent() {
     if (autoscroll && !filterText) {
-			let index = completeList.indexOf(current);
-			if (index > 0) index -= 1; //move current a bit to center
-			scrollToIndex(index);
+      let index = completeList.indexOf(current);
+      if (index > 0) index -= 1; //move current a bit to center
+      scrollToIndex(index);
       autoscroll = false;
-		}
-	}
+    }
+  }
 
-	// Event handler
-	function handlePlay(event, song) {
-		play(song);
-		autoscroll = true;
-		window.getSelection().removeAllRanges();
-		//audio.play()
-		// TODO audio.autoplay = true
-	}
+  // Event handler
+  function handlePlay(event, song) {
+    play(song);
+    autoscroll = true;
+    window.getSelection().removeAllRanges();
+    //audio.play()
+    // TODO audio.autoplay = true
+  }
 
-	function handleDblClick(e, song) {
-		jumpTo(song);
-	}
+  function handleDblClick(e, song) {
+    jumpTo(song);
+  }
 
-	function handlePlayPause(event) {
-		playPause();
-	}
+  function handlePlayPause(event) {
+    playPause();
+  }
 
-	// Life cycle
-	afterUpdate(function (x) {
-		scrollToCurrent();
-	});
+  // Life cycle
+  afterUpdate(function (x) {
+    scrollToCurrent();
+  });
 </script>
 
 <ul
@@ -100,45 +102,45 @@
     bind:end={liEnd}
     bind:scrollToIndex
   >
-		<li class="song {song.type}" on:dblclick={(e) => handleDblClick(e, song)}>
-			<span class="status-icon" />
-			<span class="spacer" />
-			<span class="name">{song.name}</span>
-			<span class="spacer" />
-			{#if song.type === PLAYED}
+    <li class="song {song.type}" on:dblclick={(e) => handleDblClick(e, song)}>
+      <span class="status-icon" />
+      <span class="spacer" />
+      <span class="name">{song.name}</span>
+      <span class="spacer" />
+      {#if song.type === PLAYED}
         <button on:click={(e) => queueSong(song, previous)}> play next </button>
-			{:else if song.type === CURRENT}
-				<button on:click={handlePlayPause}>
+      {:else if song.type === CURRENT}
+        <button on:click={handlePlayPause}>
           {#if isPaused}play{:else}pause{/if}
-				</button>
-			{:else if song.type === QUEUE}
+        </button>
+      {:else if song.type === QUEUE}
         <button on:click={(e) => resetSong(song, next)}>
           reset to playlist
         </button>
-			{:else if song.type === PREV_QUEUE}
+      {:else if song.type === PREV_QUEUE}
         <button on:click={(e) => resetSong(song, nextPrev)}>
           reset to history
         </button>
-			{:else if song.type === REMAINING}
+      {:else if song.type === REMAINING}
         <button on:click={(e) => queueSong(song, remaining)}>
           play next
         </button>
-			{/if}
-		</li>
-	</VirtualList>
+      {/if}
+    </li>
+  </VirtualList>
 </ul>
 
 <style>
-	.spacer {
-		min-width: 0.6em;
-	}
+  .spacer {
+    min-width: 0.6em;
+  }
 
-	.playlist {
-		display: flex;
-		flex-flow: column;
-		flex: 1;
-		overflow: auto;
-		display: block;
+  .playlist {
+    display: flex;
+    flex-flow: column;
+    flex: 1;
+    overflow: auto;
+    display: block;
     position: relative;
     z-index: 0;
   }
@@ -158,51 +160,51 @@
     height: 2rem;
     bottom: 0;
     box-shadow: 0 -2rem 1rem -1rem white inset;
-	}
+  }
 
-	li {
-		padding: 0.2em;
-		border-bottom: 1.1px solid currentColor;
-		display: flex;
-		align-items: center;
-		position: relative;
-		height: 45px; /*important for exact virtual list*/
-		line-height: 1;
-	}
-	li:active {
-		background: #eee;
-	}
+  li {
+    padding: 0.2em;
+    border-bottom: 1.1px solid currentColor;
+    display: flex;
+    align-items: center;
+    position: relative;
+    height: 45px; /*important for exact virtual list*/
+    line-height: 1;
+  }
+  li:active {
+    background: #eee;
+  }
 
-	li .name {
-		flex: 1;
-	}
+  li .name {
+    flex: 1;
+  }
 
   .PLAYED,
   .PREV_QUEUE {
     background: #e3e3e3;
   }
 
-	.CURRENT {
+  .CURRENT {
     border-top: 3px solid;
-		font-weight: bold;
-	}
+    font-weight: bold;
+  }
 
-	.status-icon {
-		min-width: 1.2em;
-		text-align: center;
-	}
-	.CURRENT .status-icon:before {
+  .status-icon {
+    min-width: 1.2em;
+    text-align: center;
+  }
+  .CURRENT .status-icon:before {
     content: "playing:";
-	}
-	.PLAYED .status-icon:before,
-	.QUEUE .status-icon:before,
-	.PREV_QUEUE .status-icon:before {
+  }
+  .PLAYED .status-icon:before,
+  .QUEUE .status-icon:before,
+  .PREV_QUEUE .status-icon:before {
     content: "next";
-	}
-	.PLAYED .status-icon:before {
+  }
+  .PLAYED .status-icon:before {
     content: "";
-	}
-	.PREV_QUEUE .status-icon:before {
+  }
+  .PREV_QUEUE .status-icon:before {
     content: "#next";
-	}
+  }
 </style>
